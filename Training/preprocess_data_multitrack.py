4dba75821717429ac3d7fea3a82bc3c3785114e4
KyimaLiaru <andrewharry8610@gmail.com>
2025-04-04 18:53:01 +0900

    Update Simple WaveNet Data Preprocessing for MultiTrack Generation

--- Training/preprocess_data_multitrack.py
+++ Training/preprocess_data_multitrack.py
@@ -4,6 +4,8 @@ import tarfile
 import os
 import numpy as np
 import pretty_midi
+import json
+
 
 #########################################
 # Preprocess and Save Lakh Midi Dataset #
@@ -48,34 +50,122 @@ def process_lakh_data(file):
         print(f"Error processing file: {e}")
         return None
 
-# Define the base path for the Lakh MIDI Dataset
-lakh_dataset_path = "../../../dataset/Raw/Lakh/lmd_matched.tar.gz"
-lakh_preprocess_output_path = "../../../dataset/Preprocessed/Lakh/MultiTrack"
+# # Define the base path for the Lakh MIDI Dataset
+# lakh_dataset_path = "../../../dataset/Raw/Lakh/lmd_matched.tar.gz"
+# lakh_preprocess_output_path = "../../../dataset/Preprocessed/Lakh/MultiTrack"
+#
+# # Check whether the output directory exists
+# if not os.path.exists(lakh_preprocess_output_path):
+#     os.makedirs(lakh_preprocess_output_path)
+#
+# # Open the tar.gz file and process each MIDI file
+# with tarfile.open(lakh_dataset_path, "r:gz") as tar:
+#     count = 0
+#     for member in tar.getmembers():
+#         if member.name.endswith(".mid"):
+#             # Set the output file name
+#             prefix_parts = member.name.split("/")  # e.g., 'A', 'B', etc.
+#             prefix = "".join(prefix_parts[1:4])  # three directory levels, typically alphabet letters
+#             basename = os.path.basename(member.name).replace(".mid", ".npy")
+#             output_file = os.path.join(lakh_preprocess_output_path, f"{prefix}_{basename}")
+#             if os.path.exists(output_file):
+#                 continue
+#
+#             # Extract process the data from the tar.gz file
+#             file = tar.extractfile(member)
+#             if file is not None:
+#                 result = process_lakh_data(file)
+#                 if result is not None:
+#                     # Save the preprocessed data
+#                     np.save(output_file, result)
+#                     print(f"Saved {output_file}")
+#                     count += 1
+#
+#             if count % 100 == 1:
+#                 print(f"Processed {count} files")
 
-# Check whether the output directory exists
-if not os.path.exists(lakh_preprocess_output_path):
-    os.makedirs(lakh_preprocess_output_path)
+######################################
+# Preprocess and Save NSynth Dataset #
+######################################
 
-# Open the tar.gz file and process each MIDI file
-with tarfile.open(lakh_dataset_path, "r:gz") as tar:
-    count = 0
-    for member in tar.getmembers():
-        if member.name.endswith(".mid"):
-            # Set the output file name
-            prefix_parts = member.name.split("/")  # e.g., 'A', 'B', etc.
-            prefix = "".join(prefix_parts[1:4])  # three directory levels, typically alphabet letters
-            basename = os.path.basename(member.name).replace(".mid", ".npy")
-            output_file = os.path.join(lakh_preprocess_output_path, f"{prefix}_{basename}")
+# Load and parse the nsynth.tfrecord dataset file
+def parse_tfrecord(record):
+    feature_description = {
+        'audio': tf.io.FixedLenFeature([64000], tf.float32),
+        'instrument_family': tf.io.FixedLenFeature([], tf.int64),
+        'instrument_family_str': tf.io.FixedLenFeature([], tf.string),
+        'pitch': tf.io.FixedLenFeature([], tf.int64),
+        'velocity': tf.io.FixedLenFeature([], tf.int64),
+    }
+    return tf.io.parse_single_example(record, feature_description)
 
-            # Extract process the data from the tar.gz file
-            file = tar.extractfile(member)
-            if file is not None:
-                result = process_lakh_data(file)
-                if result is not None:
-                    # Save the preprocessed data
-                    np.save(output_file, result)
-                    print(f"Saved {output_file}")
-                    count += 1
 
-            if count % 100 == 0:
-                print(f"Processed {count} files")
+def process_nsynth_data(file_path, output_base_path, dataset_type, sample_rate=16000):
+    raw_dataset = tf.data.TFRecordDataset(file_path)
+    parsed = raw_dataset.map(parse_tfrecord)
+
+    counters = {key: 0 for key in TARGET_FAMILIES}
+    for record in parsed:
+        instrument_family_str = record['instrument_family_str'].numpy().decode('utf-8')
+        instrument_source = record['instrument_source'].numpy()
+        if instrument_family_str not in TARGET_FAMILIES:
+            continue
+        elif instrument_family_str == "bass" and instrument_source == 1:
+            continue
+
+        # # Determine instrument type based on family and source
+        # instrument_family = record['instrument_family'].numpy()
+        # if instrument_family == 0:
+        #     instrument_type = "bass"
+        # elif instrument_family == 4:
+        #     instrument_type = "keyboard"
+        # elif instrument_family == 9:
+        #     instrument_type = "synth_lead"
+        # else:
+        #     continue  # Skip if not one of the desired types
+
+        # Prepare save directory
+        nsynth_preprocess_output_path = os.path.join(output_base_path, instrument_family_str)
+        if not os.path.exists(nsynth_preprocess_output_path):
+            os.makedirs(nsynth_preprocess_output_path)
+        output_file = os.path.join(nsynth_preprocess_output_path,
+                                   f"{dataset_type}_{counters[instrument_family_str]:08d}.npy")
+
+        # Process audio
+        audio = record['audio'].numpy()
+        audio = librosa.resample(audio, orig_sr=64000, target_sr=sample_rate)
+        audio = audio / np.max(np.abs(audio))
+
+        # Save preprocessed data
+        np.save(output_file, {
+            'audio': audio,
+            'instrument_family_str': instrument_family_str,
+            'pitch': record['pitch'].numpy(),
+            'velocity': record['velocity'].numpy(),
+        })
+
+        print(f"Saved {output_file}")
+        counters[instrument_family_str] += 1
+
+    for key, value in counters.items():
+        print(f"Saved {value} samples as instrument {key}.")
+
+
+
+# Target instrument families
+TARGET_FAMILIES = {
+    "bass": "bass",
+    "keyboard": "keyboard",
+    "synth_lead": "synth_lead"
+}
+
+nsynth_input_paths = {
+    "train": "../../../../dataset/Raw/NSynth/nsynth-train.tfrecord",
+    "valid": "../../../../dataset/Raw/NSynth/nsynth-valid.tfrecord",
+    "test": "../../../../dataset/Raw/NSynth/nsynth-test.tfrecord"
+}
+
+nsynth_output_path = "../../../../dataset/Preprocessed/NSynth/16000"
+
+for dtype, path in nsynth_input_paths.items():
+    process_nsynth_data(path, nsynth_output_path, dtype, 16000)
