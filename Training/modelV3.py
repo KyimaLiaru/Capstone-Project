ca252ca8004e42b7642920b095189acd1a7d1a69
KyimaLiaru <andrewharry8610@gmail.com>
2025-04-08 02:09:52 +0900

    Update Simple MuseGAN Training Code Add Checkpoints

--- Training/modelV3.py
+++ Training/modelV3.py
@@ -1,13 +1,16 @@
 import os
 import io
+import json
 import tarfile
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv1D, LSTM, Dense, Flatten, Reshape, Dropout, Concatenate
 from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
 import pretty_midi
 import matplotlib.pyplot as plt
 import librosa
+import pandas as pd
 
 
 ##################################################
@@ -80,6 +83,7 @@ def load_lakh_data(dataset_path, batch_size, data_type, split_ratio=0.8):
 def load_data_from_directory(data_dir, file_list, batch_size):
     while True:
         for i in range(0, len(file_list), batch_size):
+            print(i)
             batch_files = file_list[i:i + batch_size]
             batch_inputs = []
 
@@ -97,12 +101,14 @@ def load_data_from_directory(data_dir, file_list, batch_size):
             yield tuple(inputs), output
 
 # Function to plot training history
-def plot_training_history(history, path):
-    # Plot accuracy
+def plot_training_history(csv_path, save_path):
+    df = pd.read_csv(csv_path)
     plt.figure(figsize=(12, 5))
+
+    # Plot accuracy
     plt.subplot(1, 2, 1)
-    plt.plot(history.history['accuracy'], label='Train Accuracy')
-    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.plot(df['accuracy'], label='Train Accuracy')
+    plt.plot(df['val_accuracy'], label='Validation Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.title('Model Accuracy')
@@ -110,27 +116,35 @@ def plot_training_history(history, path):
 
     # Plot loss
     plt.subplot(1, 2, 2)
-    plt.plot(history.history['loss'], label='Train Loss')
-    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.plot(df['loss'], label='Train Loss')
+    plt.plot(df['val_loss'], label='Validation Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Model Loss')
     plt.legend()
 
     plt.tight_layout()
-
-    plt.savefig(path, dpi=300)
+    plt.savefig(save_path, dpi=300)
     plt.show()
 
 
 # Paths
-musegan_save_path = "../../trained_model/musegan"
+musegan_save_path = "../../trained_model/musegan.h5"
+musegan_checkpoint_path = "../../trained_model/musegan_checkpoints.h5"
+musegan_log_path = "../../trained_model/musegan_training_log.csv"
 trained_musegan_path = "../../trained_model/musegan.h5"
-# lakh_dataset_path = "../../../../dataset/Preprocessed/Lakh/MultiTrack-ver3.tar.gz"
+
+training_history_path = "../../trained_model/training_history.json"
+history = {"loss": [], "accuracy": [], "val_loss": [], "val_accuracy": []}
+
+if os.path.exists(training_history_path):
+    with open(training_history_path, "r") as f:
+        history = json.load(f)
+    print("loaded previous training history.")
+
+# lakh_dataset_path = "../../../dataset/Preprocessed/Lakh/MultiTrack-ver3.tar.gz"
 lakh_data_path = "../../../dataset/Preprocessed/Lakh/MultiTrack"
 result_plot_path = "../../../Result/Performance/performance.png"
-
-# Load Extracted Lakh MIDI data
 
 # Define model parameters
 batch_size = 32
@@ -151,17 +165,26 @@ train_batch = load_data_from_directory(lakh_data_path, train_files, batch_size)
 valid_batch = load_data_from_directory(lakh_data_path, valid_files, batch_size)
 
 # Check if trained model already exists
-# if os.path.exists(trained_musegan_path):
+if os.path.exists(trained_musegan_path):
 # if False:
-#     musegan = tf.keras.models.load_model(trained_musegan_path)
-#     print("MuseGAN model successfully loaded.")
-#
-# else:
-print("MuseGAN model not found, building MuseGAN model...")
+    musegan = tf.keras.models.load_model(trained_musegan_path)
+    print("MuseGAN model successfully loaded.")
+
+elif os.path.exists(musegan_checkpoint_path):
+    musegan = tf.keras.models.load_model(musegan_checkpoint_path)
+    print("MuseGAN model successfully loaded from checkpoint.")
+else:
+    musegan = build_musegan()
+    print("MuseGAN model not found, building new MuseGAN model...")
+
 # Build MuseGAN Model
 print("MuseGAN model summary:")
-musegan = build_musegan()
 musegan.summary()
+
+callbacks = [
+    ModelCheckpoint(musegan_checkpoint_path, monitor='val_loss', save_best_only=False, save_weights_only=False, verbose=1),
+    CSVLogger(musegan_log_path, append=True)
+]
 
 # Train MuseGAN Model
 print("MuseGAN train start...")
@@ -171,10 +194,11 @@ lakh_history = musegan.fit(
     epochs=epochs,
     batch_size=batch_size,
     validation_data=valid_batch,
-    validation_steps=validation_steps
+    validation_steps=validation_steps,
+    callbacks=callbacks
 )
 
-plot_training_history(lakh_history, result_plot_path)
+plot_training_history(musegan_log_path, result_plot_path)
 
 musegan.save(musegan_save_path)
 
