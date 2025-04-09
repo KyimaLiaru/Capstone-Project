a659d816ab2db94854f62a7d561ff5f6a46a38c1
Kyima <andrewharry8610@gmail.com>
2025-04-10 01:17:21 +0900

    Update Simplified MuseGAN Preprocessing Code

--- Training/preprocess_data_multitrack.py
+++ Training/preprocess_data_multitrack.py
@@ -17,7 +17,11 @@ def extract_instrument_roll(pm, program_range, drum=False):
         if drum != inst.is_drum:
             continue
         if drum and inst.is_drum:
-            print("found drum instrument for drum track")
+            if drum and inst.is_drum:
+                print("found drum instrument")
+                for note in inst.notes:
+                    print(
+                        f" → Pitch: {note.pitch}, velocity: {note.velocity}, start: {note.start:.2f}, end: {note.end:.2f}")
         if drum or inst.program in program_range:
             inst_roll = inst.get_piano_roll(fs=16).T[:512, :128]
             inst_roll = (inst_roll > 0).astype(float)
